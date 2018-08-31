120cc217a04c5a73c4d91ce4ea422849816c0cf3
tianjinkun <tianjinkun@spyder.link>
2018-08-31 10:22:56 +0800

    修复一个视图键值缺少的Bug

--- APP/Appointment/Model/MyAppointmentViewModel.class.php
+++ APP/Appointment/Model/MyAppointmentViewModel.class.php
@@ -4,7 +4,7 @@ use Think\Model\ViewModel;
 
 class MyAppointmentViewModel extends ViewModel{
     protected $viewFields = array(
-        'appointment' => array('aid','guest_id','fixer_id','model','issues','addtime','edittime','status','_type'=>'LEFT'),
+        'appointment' => array('aid','guest_id','fixer_id','model','issues','addtime','edittime','status','result','_type'=>'LEFT'),
         'garden_users'  => array('uid','truename'=>'fixer_name','_on'=>'garden_users.uid=appointment.fixer_id'),
     );
 }
