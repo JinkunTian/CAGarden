4c667231336636503b1973351fe67531508c0700
yhy <1072365573@qq.com>
2018-10-16 17:22:48 +0800

    添加首页随机显示后花园成员方法,添加首页反馈方法

--- APP/Index/Controller/IndexController.class.php
+++ APP/Index/Controller/IndexController.class.php
@@ -2,7 +2,31 @@
 namespace Index\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-    public function index(){
-        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>[ 您现在访问的是Index模块的Index控制器 ]</div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+	public function index(){
+
+
+		M('common_count')->where(array('site_id'=>1))->setInc('visit_count',1);
+
+		// $common_count=where()->find();
+		// $visit=$common_count['visit_count']+1;
+		// $data = array(
+	 //            'visit_count' => $visit,
+	 //        );
+		// M('common_count')->where(array('site_id' => 1))->save($data);
+		$common_count=M('common_count')->where(array('site_id'=>1))->find();
+		$best=M('garden_users')->order('rand()')->limit(3)->select();
+		//$best=M('garden_users')->order('last_login desc')->limit(3)->select();
+		$this->assign('best',$best);
+		$this->assign('common_count',$common_count);
+		$this->display();
+    }
+    public function feedback(){
+    	$data=array(
+    		'name'=>I('name'),
+    		'email'=>I('email'),
+    		'message'=>I('message'),
+    	);
+    	M('index_feedback')->add($data);
+    	$this->success('反馈成功！');
     }
 }
