01b3c84fd9304990132d93868c7337b70b733a88
tianjinkun <tianjinkun@spyder.link>
2018-11-04 17:21:42 +0800

    添加运行日志功能

--- APP/Garden/Controller/CommonController.class.php
+++ APP/Garden/Controller/CommonController.class.php
@@ -7,5 +7,15 @@ class CommonController extends Controller {
         if (!(isset($_SESSION['id'])&&isset($_SESSION['6c440f695619e361040767ac9f6fb619'])&&isset($_SESSION['name']))) {
             $this->redirect('/Garden/Login');
         }
+        $logs=array(
+        	'part'=>1,
+        	'truename'=>session('name'),
+        	'username'=>session('username'),
+        	'ip'=>get_client_ip(),
+        	'agent'=>$_SERVER["HTTP_USER_AGENT"],
+        	'url'=>$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"],
+        	'date' => date('y-m-d H:i:s'),
+        );
+        M('system_logs')->add($logs);
     }
 }
