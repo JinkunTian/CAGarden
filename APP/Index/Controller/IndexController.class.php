c574f8d7ffa1de33bb0c1b713fb8a11664fbed17
JinkunTian <tianjinkun@spyder.link>
2019-06-10 14:39:39 +0800

    将协会成员也统计到总注册会员中并在首页展示

--- APP/Index/Controller/IndexController.class.php
+++ APP/Index/Controller/IndexController.class.php
@@ -9,7 +9,7 @@ class IndexController extends Controller {
 		$base_count=M('common_count')->where(array('site_id'=>1))->find();
       	//基础数据，在老系统中，不再做数据导入，只将老记录相加后展示
 		$common_count['comment_count']=$base_count['comment_count']+M('appointment_comment')->count();
-      	$common_count['members_count']=$base_count['members_count']+M('appointment_users')->count();
+      	$common_count['members_count']=$base_count['members_count']+M('appointment_users')->count()+M('garden_users')->count;
       	$common_count['fix_count']=$base_count['fix_count']+M('appointment')->where(array('status'=>3))->count();
       	$common_count['visit_count']=$base_count['visit_count'];    
       
