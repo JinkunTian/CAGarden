bb4dee6edb8a55ab3b728af0922a6faea2ee786a
JinkunTian <tianjinkun@spyder.link>
2019-06-14 11:54:20 +0800

    统计预约模块运行日志

--- APP/Appointment/Controller/CommonController.class.php
+++ APP/Appointment/Controller/CommonController.class.php
@@ -5,7 +5,26 @@ class CommonController extends Controller {
     //自动运行，判断如果没有登录则跳转到登录页面
     public function _initialize() {
         if (!(isset($_SESSION['guest']) && isset($_SESSION['guest_id']) && isset($_SESSION['name']) )) {
+            $logs=array(
+                'part'=>3,
+                'truename'=>'游客',
+                'username'=>'游客',
+                'ip'=>get_client_ip(),
+                'agent'=>$_SERVER["HTTP_USER_AGENT"],
+                'url'=>$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"],
+                'date' => date('y-m-d H:i:s'));
+            M('system_logs')->add($logs);
             $this->redirect('/Appointment/Login');
+        }else{
+            $logs=array(
+                'part'=>3,
+                'truename'=>session('name'),
+                'username'=>session('number'),
+                'ip'=>get_client_ip(),
+                'agent'=>$_SERVER["HTTP_USER_AGENT"],
+                'url'=>$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"],
+                'date' => date('y-m-d H:i:s'));
+            M('system_logs')->add($logs);
         }
     }
 }
