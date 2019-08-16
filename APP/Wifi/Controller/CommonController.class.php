4e43bc78029909401634f90a4a1f3f9be7ee2823
JinkunTian <tianjinkun@spyder.link>
2019-08-17 02:01:40 +0800

    添加iKuai WiFi接入审计

---
+++ APP/Wifi/Controller/CommonController.class.php
@@ -0,0 +1,31 @@
+<?php
+namespace Wifi\Controller;
+use Think\Controller;
+class CommonController extends Controller {
+    //自动运行，判断如果没有登录则跳转到登录页面
+    public function _initialize() {
+        if (!(isset($_SESSION['username']) && isset($_SESSION['id']) && isset($_SESSION['name']) )) {
+            $logs=array(
+                'part'=>5,
+                'truename'=>'游客',
+                'username'=>'游客',
+                'ip'=>get_client_ip(),
+                'agent'=>$_SERVER["HTTP_USER_AGENT"],
+                'url'=>$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"],
+                'date' => date('y-m-d H:i:s'));
+            M('system_logs')->add($logs);
+            session('req_url',$_SERVER["REQUEST_URI"]);
+            $this->redirect('/Login');
+        }else{
+            $logs=array(
+                'part'=>5,
+                'truename'=>session('name'),
+                'username'=>session('username'),
+                'ip'=>get_client_ip(),
+                'agent'=>$_SERVER["HTTP_USER_AGENT"],
+                'url'=>$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"],
+                'date' => date('y-m-d H:i:s'));
+            M('system_logs')->add($logs);
+        }
+    }
+}
---
+++ APP/Wifi/Controller/IndexController.class.php
@@ -0,0 +1,56 @@
+<?php
+namespace Wifi\Controller;
+use Think\Controller;
+/***
+ * @Author:      田津坤
+ * @Email:       tianjinkun@spyder.link
+ * @GitHub:      https://github.com/JinkunTian
+ * @DateTime:    2018年8月21日14:45:47
+ * @Description: 计算机协会WiFi接入审计控制器
+ ***/
+class IndexController extends CommonController {
+
+    private function getData($url, $token = "")
+	{
+		$ch = curl_init();
+		$timeout = 3;
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			'token:' . $token
+		));
+		$handles = curl_exec($ch);
+		curl_close($ch);
+		return json_decode($handles,true);
+    }
+    
+    /**
+     * index方法列出用户所有预约信息
+     */
+    public function index(){
+        $wifi_log=array(
+            'mac'=>I('mac'),
+            'ip'=>I('user_ip'),
+            'timestamp'=>date("Y-m-d H:i:s",I('timestamp')),
+            'username'=>session('username'),
+            'truename'=>session('name')
+        );
+        M('wifi')->add($wifi_log);
+        $base_url='https://portal.ikuai8-wifi.com/Action/webauth-up?type=20';
+        $token=md5('user_ip='.$wifi_log['ip'].'&timestamp='.I('timestamp').'&mac='.$wifi_log['mac'].'&upload=0&download=0&key='.C('iKuai_APPKEY'));
+        $url=$base_url.'&user_id='.$wifi_log['username'].'&custom_name='.$wifi_log['name'].'&user_ip='.$wifi_log['ip']."&timestamp=".I('timestamp').'&mac='.$wifi_log['mac'].'&upload=0&download=0&token='.$token;
+        $resault=$this->getData($url);
+        if($resault['errmsg']=='认证成功'){
+            $this->success('WiFi接入成功！','http://www.mecca.org.cn',2);
+        }else{
+            $this->error('WiFi接入失败，请暂时访问内网资源！','http://www.mecca.org.cn');
+        }
+    }
+    
+    /**
+     * 别看了，没有deleteAppointment方法的，不允许用户单方面删除记录的
+     * 年纪轻轻的，学锤子计算机？有头发不好吗！
+     */
+}
